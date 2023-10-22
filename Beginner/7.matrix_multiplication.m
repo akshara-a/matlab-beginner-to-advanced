6f4f13285fce938147fd053235eb8872b9a8152d
Akshara A <68069921+akshara-a@users.noreply.github.com>
2023-10-23 00:05:05 +0530

    Create 7.matrix_multiplication.m

---
+++ Beginner/7.matrix_multiplication.m
@@ -0,0 +1,7 @@
+% Matrix Multiplication
+a = [1 2 3; 4 5 6]; 
+b = [1 2 3; 4 5 6; 7 8 9];
+disp(a*b)
+
+% Note 
+% Check that the number of columns in the first matrix matches the number of rows in the second matrix.
