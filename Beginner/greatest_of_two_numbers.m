328af02053f792a8d06fc3714e891306a21c0b45
Akshara A <68069921+akshara-a@users.noreply.github.com>
2023-10-22 23:48:30 +0530

    Create greatest_of_two_numbers.m

---
+++ Beginner/greatest_of_two_numbers.m
@@ -0,0 +1,15 @@
+% Greatest number 
+
+x = 10; 
+y = 20; 
+
+if(x > y)
+    disp("x is greater"); 
+elseif(x < y)
+        disp("y is greater"); 
+else
+    disp('x and y are equal'); 
+end
+
+% References 
+% https://www.geeksforgeeks.org/matlab-conditional-statements/
